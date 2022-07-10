1d86eb85fdf4ecd286a8ef1d2bb867151ed6f3f8
stufit <71133204+stufit@users.noreply.github.com>
2022-07-10 13:54:17 +0900

    Feat: 기본타입 - 문자열,숫자,배열

---
+++ class-note/1_type-basic.ts
@@ -0,0 +1,13 @@
+// JS 문자열 선언
+//var str = 'hello';
+
+// TS 문자열 선언
+let str: string = 'hello';
+// TS 숫자 선언
+let num: number = 10;
+// TS 배열 선언
+let arr: Array<number> = [1,2,3]; // 배열선언해주고 배열안에는 number만 들어갈 수 있음.
+let heroes: Array<string> = ['rhksdud','rhksghd','chang'] // 배열 안에는 string만 들어갈 수 있음.
+let items: number[] = [1,2,3]; // 배열표현은 이렇게 표현할 수도 있음. 배열 안에는 number만 가능
+
+
