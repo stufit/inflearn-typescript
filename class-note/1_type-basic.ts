94ee6e1f16e2935404c9636bd1c841f1efe6a083
stufit <71133204+stufit@users.noreply.github.com>
2022-07-10 14:00:07 +0900

    Feat: 기본타입- 튜플,객체,진위값

--- class-note/1_type-basic.ts
+++ class-note/1_type-basic.ts
@@ -9,5 +9,22 @@ let num: number = 10;
 let arr: Array<number> = [1,2,3]; // 배열선언해주고 배열안에는 number만 들어갈 수 있음.
 let heroes: Array<string> = ['rhksdud','rhksghd','chang'] // 배열 안에는 string만 들어갈 수 있음.
 let items: number[] = [1,2,3]; // 배열표현은 이렇게 표현할 수도 있음. 배열 안에는 number만 가능
+// TS 튜플 선언
+let address: [string, number] = ['rhksdud',30]; // 자리수까지 타입을 선언해줄 수 있음.
+// TS 객체
+let obj: object = {};
+// TS 객체 - 첫번째 선언방법
+let person_v1: object = {
+  name: 'rhksdud',
+  age: 32,
+};
+// TS 객체 - 두번째 선언방법 => 들어갈 변수명과 타입을 미리 지정
+let person_v2: {name: string, age: number} = {
+  name: 'rhksdud',
+  age: 32,
+}
+// TS 진위값
+let show: boolean = true;
+
 
 
