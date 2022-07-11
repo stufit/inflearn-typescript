e6ad9922530b499d7573288ae14f7e8a1139e7e9
이관영 <stufit@marketboro.com>
2022-07-12 01:05:22 +0900

    함수의 인자를 정의하는 인터페이스

--- class-note/3_interface.ts
+++ class-note/3_interface.ts
@@ -3,7 +3,19 @@ interface User {
   name: string;
 }
 
+// 변수에 인터페이스 활용
 var seho: User = {
   age: 3,
   name: '관영'
 }
+
+// 함수에 인터페이스 활용
+function getUser(user: User) {
+  console.log(user)
+}
+const capt = {
+  name: '마켓보로',
+  age: 32
+}
+getUser(capt);
+
