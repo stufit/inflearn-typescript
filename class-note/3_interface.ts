0ce76ee896bafc4c80379f6239a8297da287f343
이관영 <stufit@marketboro.com>
2022-07-12 01:13:26 +0900

    Feat: 인덱싱 방식을 정의하는 인터페이스

--- class-note/3_interface.ts
+++ class-note/3_interface.ts
@@ -27,3 +27,12 @@ interface SumFunction {
 const sum_v2 = function (a:number, b: number) {
   return a+b;
 }
+
+// 인덱싱
+// 인덱스는 배열[0] 처럼 숫자를 의미하기 때문에 number를 넣어주며, 반환값 즉 해당 인덱싱의 값 타입을 : string, number 등으로 선언해줄 수 있다.
+interface StringArray {
+  [index: number]: string; // 배열 안의 값은 스트링으로 선언
+}
+var arr_v1: StringArray = ['a','b','c'];
+// arr_v1[0] = 3 // 배열 안의 값이 스트링으로 선언되어있기 때문에 숫자를 넣으면 에러
+
